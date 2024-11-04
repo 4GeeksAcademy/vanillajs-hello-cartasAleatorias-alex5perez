5cfe6b43abeb79aea3ce8f34492d882c0b8b522a
Alex Perez Rubio <71727357+alex5perez@users.noreply.github.com>
2024-11-04 19:16:20 +0000

    [Se añade la funcionalidad del tamaño] app.js: □ Nueva funcion que utlizare en un onclick en el Html □ Constantes para agarrar el id del Html □ Cambiamos la altura y la anchura en px

--- src/app.js
+++ src/app.js
@@ -1,8 +1,42 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const valor = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+  const palos = ["♦", "♥", "♠", "♣"];
+
+  const indexNumber = Math.floor(Math.random() * valor.length);
+  const indexPalos = Math.floor(Math.random() * palos.length);
+
+  const valorRandom = `${valor[indexNumber]}`;
+  const palosRandom = `${palos[indexPalos]}`;
+
+  document.getElementById("number").innerText = valorRandom;
+  document.getElementById("top-card").innerText = palosRandom;
+  document.getElementById("bottom-card").innerText = palosRandom;
+
+  const topCard = document.getElementById("top-card");
+  const bottomCard = document.getElementById("bottom-card");
+
+  if (palosRandom === "♦" || palosRandom === "♥") {
+    topCard.style.color = "red";
+    bottomCard.style.color = "red";
+  }
 };
+
+setTimeout(function() {
+  window.location.reload();
+}, 10000);
